04e8e891af7609137ac7b7ea7e9bba18e6c67cd6
AnastasiaValishina <azhidenova@gmail.com>
2021-04-09 23:07:25 +0200

    level transitions

---
+++ Berry Match/Assets/Scripts/UI Scripts/ConfirmPopup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConfirmPopup : MonoBehaviour
+{
+    public Image[] stars;
+    public int level;
+    void Start()
+    {
+        ActivateStars();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnCloseClick()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnPlayClicked()
+    {
+        PlayerPrefs.SetInt("Current Level", level - 1);
+        SceneManager.LoadScene(1);
+    }
+
+    void ActivateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].enabled = false;
+        }
+    }
+}
---
+++ Berry Match/Assets/Scripts/UI Scripts/LevelItem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelItem : MonoBehaviour
+{
+    public Image[] stars;
+    public Text levelText;
+    public int level;
+    public GameObject confirmPopup;
+    public bool isActive;
+    public Image shadow;
+
+
+ //   Image buttonImage;
+    Button itemButton;
+
+    void Start()
+    {
+    //    buttonImage = GetComponent<Image>();
+        itemButton = GetComponent<Button>();
+        ActivateStars();
+        ShowLevel();
+        SetSprite();
+    }
+
+    void SetSprite()
+    {
+        if (isActive)
+        {
+            shadow.gameObject.SetActive(false);
+            itemButton.enabled = true;
+        }
+        else
+        {
+            shadow.gameObject.SetActive(true);
+            itemButton.enabled = false;
+        }
+    }
+
+    public void ShowConfirmPopup(int level)
+    {
+        confirmPopup.GetComponent<ConfirmPopup>().level = level;
+        confirmPopup.SetActive(true);
+    }
+
+    void ShowLevel()
+    {
+        levelText.text = "" + level;
+    }
+
+    void ActivateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].enabled = false;
+        }
+    }
+}
